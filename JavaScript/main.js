c3cbe307e69183a96bc47bf991c7fb70eaa5b944
Paulovcarraro <paulovcarraro@gmail.com>
2025-10-17 10:50:11 -0300

    Criação daspáginas aluno e gestor .html, além de adição de validação de senha de no minimo 6 caracteres

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,6 +1,6 @@
 // Aguarda o carregamento completo da página
 document.addEventListener("DOMContentLoaded", function () {
-  const btnLogin = document.querySelector("btn-login");
+  const btnLogin = document.querySelector(".btn-login");
 
   btnLogin.addEventListener("click", function () {
     const email = document.getElementById("email").value.trim();
@@ -13,11 +13,17 @@ document.addEventListener("DOMContentLoaded", function () {
       return;
     }
 
+    // Verficação do tamanho minimo da senha (6 digitos)
+    if (senha.length < 6) {
+      alert("A senha deve ter no minimo 6 caracteres.");
+      return;
+    }
+
     // Direciona o usuário conforme o escolhido no selct
     if (tipo === "gestor") {
-      window.location.href = "./gestor.html"; //Há criar o arquivo html
+      window.location.href = "./gestor.html";
     } else if (tipo === "aluno") {
-      window.location.href = "aluno.html"; //Há criar o arquivo html
+      window.location.href = "./aluno.html";
     } else {
       alert("Selecione um tipo de usuário válido.");
     }
